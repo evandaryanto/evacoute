e3b1795c1326b47661e8d807f854cace72f81c76
Michinggun <michinggun@gmail.com>
2015-10-24 18:30:23 +0700

    changed splash activity + search icon

--- app/src/main/java/com/merdekabyte/evacoute/ui/activity/DrawerActivity.java
+++ app/src/main/java/com/merdekabyte/evacoute/ui/activity/DrawerActivity.java
@@ -61,7 +61,7 @@ public class DrawerActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.drawer, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
